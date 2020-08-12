c9bb4eb04c3ba09e6d2cf5bb6631e63aa2e9c10b
zahrabashir98 <zahra_9877@yahoo.com>
2020-08-13 03:47:00 +0430

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Router from './Router';
-import * as serviceWorker from "./serviceWorker";
-
-ReactDOM.render(<Router />, document.getElementById('SiteRoot'));
-serviceWorker.register();
